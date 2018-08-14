1033345bd38f811e366adb011895570c4ea3c68d
AustinP <rockmanvnx6@gmail.com>
2018-08-15 00:55:40 +1000

    fixed

--- init.py
+++ init.py
@@ -15,6 +15,19 @@ def console(command):
     command = input("Type /help to see valid commands: ")
     return command
 
+def check(sub_display,des_display,tol_display):
+    if (sub_display == "" or des_display == "" or tol_display == "" or len(sub_display) < 2 or len(sub_display) > 128
+        or len(des_display) < 2 or len(des_display) > 128 or len(tol_display) < 2 or len(tol_display) > 128):
+        print("")
+        print("* Fields can't be empty and must be between 2 and 128 characters")
+        print("")
+        sub_display = input("Input subject: ")
+        des_display = input("Input description: ")
+        tol_display = input("Tooltip when hover RMIT logo: ")
+        return check(sub_display,des_display,tol_display)
+    else:
+        return sub_display,des_display,tol_display
+
 command = console(command)
 
 while True:
@@ -23,6 +36,7 @@ while True:
         print("")
         print("1. Type /update to change the status")
         print("2. Type /clear to clear the status")
+        print("3. Type /quit to quit the program")
         print("")
         command = console(command)
 
@@ -32,12 +46,7 @@ while True:
         des_display = input("Input description: ")
         tol_display = input("Tooltip when hover RMIT logo: ")
 
-        if(sub_display=="" or des_display=="" or tol_display==""):
-            print("")
-            print("Fields can't be empty")
-            sub_display = input("Input subject: ")
-            des_display = input("Input description: ")
-            tol_display = input("Tooltip when hover RMIT logo: ")
+        sub_display,des_display,tol_display = check(sub_display,des_display,tol_display)
 
         RPC.update(state="Description: " + des_display, details="" + sub_display, start=int(time.time()),large_text=tol_display, large_image="rmit-l")
         print("Updated Successfully !!!")
@@ -50,6 +59,12 @@ while True:
         print("Cleared the status")
         print("")
         command = console(command)
+
+    elif (command == "/quit"):
+        print("")
+        print("Thank you for using !")
+        os._exit(1)
+
     else:
         print("")
         print("Invalid command!")
